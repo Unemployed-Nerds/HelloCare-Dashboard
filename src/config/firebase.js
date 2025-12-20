bc2a28bf4e7719b96d3a474cb20fe565d61e52d8
Sravan <miniclip1956@gmail.com>
2025-12-21 04:26:24 +0530

    use new api key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC3QQH_1p479bzZZgrlfUkw0a0_GxVE9G4",
+  authDomain: "hellocare-fd1b7.firebaseapp.com",
+  projectId: "hellocare-fd1b7",
+  storageBucket: "hellocare-fd1b7.firebasestorage.app",
+  messagingSenderId: "843661331522",
+  appId: "1:843661331522:web:e7cf2f5abf426367b871db",
+  measurementId: "G-1PE6ZEKK2T"
 };
 
 const app = initializeApp(firebaseConfig);
